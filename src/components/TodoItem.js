bd825a390a274e63bfd3033421ebb793e62efb61
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-04-18 18:49:35 +0700

    Add files via upload

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,30 @@
 // I want to load a whole new component
 // With ES7-REACT-REDUX EXTENSION, GENERATE COMPONENT BY QUICKLY USE rce+tab
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class TodoItem extends Component {
+  
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
   render() {
     // In this component we must acknowledge and use todo as parameter. NOT todos object itself
     return (
-      <div>
+      <div style={this.getStyle()}>
         <p>{ this.props.todo.title }</p>
       </div>
     )
   }
 }
-
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+ 
 export default TodoItem
