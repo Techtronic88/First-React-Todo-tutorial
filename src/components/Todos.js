e3ba997a3f82b6a04d0566d16bc298f67aa3f583
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-05-30 21:45:41 +0700

    Add files via upload
    Changes made to this commit
    - Refactored the todos changed it to functional component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem'; // Here we are just loading a whole new component
-import PropTypes from 'prop-types';
+import React from 'react';
+import TodoItem from './TodoItem'; 
 
+// We will pass the methods and states of MAIN component down to TodoItem.js component from here
+const Todos = (props) => (  
 
-class Todos extends Component {
-
-  // Again we have to go up to another level 
-  // I have assigned the this.props.markComplete below
-  // We will complete the markComplete in the state level of the App Component
-  // markComplete = () => {
-  //   console.log('Hello')
-  // }
-
-  render() {
-      // You will get hello 3 times because you have imported TodoItem.js components
-      // todo papameter IS a props so we can pass it to TodoItem.js component
-      // We can then jump to TodoItem.js and use the todo parameter
-      return this.props.todos.map((todo) => (<TodoItem key={todo.id} todo={ todo } 
-      markComplete={this.props.markComplete} 
-      delTodo= {this.props.delTodo}/>)); 
+    props.todos.map((todo) => (<TodoItem key={todo.id} todo={todo} 
+    markComplete={props.markComplete} 
+    delTodo= {props.delTodo}/>))
      
-    }
-}
+  )    
+export default Todos
 
-// ADDING PROP TYPES
-Todos.propTypes = {
-  todos: PropTypes.array.isRequired
-}
-export default Todos;
